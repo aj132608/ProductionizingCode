6232d82a68588b9236c89ed4ee93365d81e1ad9b
Alex Jirovsky <aj132608@gmail.com>
2021-07-08 10:11:45 -0500

    Updated modelserving file.

--- APIs/modelserving.py
+++ APIs/modelserving.py
@@ -1,20 +1,19 @@
 from flask import Flask, request
-from tensorflow.keras import load_model
+from tensorflow.keras.models import load_model
 import sys
+import json
 
-# Get your flask app object
 app = Flask(__name__)
-
-# Load model from local FS on server start
 model = load_model(sys.argv[1])
 
 
-@app.serve("/", methods=["POST"])
+@app.route("/", methods=["POST"])
 def predict():
-    inputs = request.data
-    preds = model.predict(inputs)
-    return preds
+    inputs = json.loads(request.data)["instances"]
+    preds = model.predict(inputs).tolist()
+    return {
+            "predictions": preds
+    }
 
 
-if __name__ == '__main__':
-    app.run()
+app.run(host="0.0.0.0", port=5000)
